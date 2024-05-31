61e445c0ece9050d7103de8a725e590fedc0d6aa
Zalfy Putra <zalfy.putra@ui.ac.id>
2024-06-01 00:42:13 +0700

    Finished Login & Register

--- backend/src/server/handler.js
+++ backend/src/server/handler.js
@@ -1,66 +1,41 @@
-const { Storage } = require('@google-cloud/storage');
+const admin = require('./firebase');
+const bcrypt = require('bcrypt');
 
-const storage = new Storage();
-const bucketName = 'artnaon-bucket-991';
-
-const uploadHandler = async (file) => {
-    const getOrCreateBucket = async (bucketName) => {
-        const bucket = storage.bucket(bucketName);
-        try {
-            const [metadata] = await bucket.getMetadata();
-            console.log(`Bucket ${metadata.name} sudah tersedia!`);
-            return bucket;
-        } catch (error) {
-            const optionsCreateBucket = {
-                location: 'ASIA-SOUTHEAST2'
-            };
-            await storage.createBucket(bucketName, optionsCreateBucket);
-            console.log(`${bucketName} bucket created successfully`);
-            return bucket;
-        }
-    };
-
-    const bucket = await getOrCreateBucket(bucketName);
-    const customMetadata = {
-        contentType: file.hapi.headers['content-type'],
-        metadata: {
-            type: "header-logo"
-        }
-    };
-
-    const optionsUploadObject = {
-        destination: 'bangkit.png',
-        metadata: customMetadata
-    };
-
+const registerHandler = async (request, h) => {
+    const { email, password } = request.payload;
     try {
-        const fileBuffer = file._data;
-        const fileUpload = bucket.file('bangkit.png');
-        await fileUpload.save(fileBuffer, optionsUploadObject);
-        console.log(`${file.hapi.filename} uploaded to ${bucketName} bucket`);
-        return `${file.hapi.filename} uploaded to ${bucketName} bucket`;
-    } catch (uploadError) {
-        console.error(`Gagal mengupload ${file.hapi.filename}:`, uploadError.message);
-        throw new Error(`Gagal mengupload ${file.hapi.filename}: ${uploadError.message}`);
+      const userRecord = await admin.auth().createUser({
+        email,
+        password,
+      });
+      return h.response({ message: 'User successfully created!', email: userRecord.email }).code(201);
+    } catch (error) {
+      return h.response({ message: error.message }).code(400);
     }
 };
 
-// Fungsi download single object
-const downloadHandler = async (request, h) => {
-    const { fileName, destination } = request.payload;
+const loginHandler = async (request, h) => {
+    const { email, password } = request.payload;
     try {
-        const bucket = await getOrCreateBucket(bucketName);
-        const optionsDownloadObject = {
-            destination: destination,
-        };
-
-        await bucket.file(fileName).download(optionsDownloadObject);
-        console.log(`${fileName} downloaded to ${destination}`);
-        return h.response(`${fileName} downloaded successfully to ${destination}`).code(200);
-    } catch (downloadError) {
-        console.error(`Gagal mendownload ${fileName}:`, downloadError.message);
-        return h.response(`Gagal mendownload ${fileName}: ${downloadError.message}`).code(500);
+      const user = await admin.auth().getUserByEmail(email);
+      return h.response({ message: 'Login success' }).code(200);
+    } catch (error) {
+      return h.response({ message: 'Login failed', error: error.message }).code(400);
     }
 };
 
-module.exports = { uploadHandler, downloadHandler };
+const forgotPassword = async (request, h) => {
+    const { email } = request.payload;
+    try {
+        await admin.auth().generatePasswordResetLink(email);
+        return h.response({ message: 'Password reset email sent' }).code(200);
+    } catch (error) {
+        return h.response({ message: 'Error sending password reset email', error }).code(400);
+    }
+};
+
+module.exports = {
+  registerHandler,
+  loginHandler,
+  forgotPassword,
+};
--- backend/src/server/routes.js
+++ backend/src/server/routes.js
@@ -1,31 +1,24 @@
-const { uploadHandler, downloadHandler } = require('./handler');
+const { 
+    registerHandler, 
+    loginHandler, 
+    forgotPassword
+} = require('./handler');
 
 const routes = [
     {
         method: 'POST',
-        path: '/upload',
-        options: {
-            payload: {
-                output: 'stream',
-                parse: true,
-                multipart: true,
-                allow: 'multipart/form-data'
-            },
-            handler: async (request, h) => {
-                const { image } = request.payload;
-                try {
-                    const message = await uploadHandler.uploadFile(image);
-                    return h.response({ message }).code(200);
-                } catch (err) {
-                    return h.response({ error: err.message }).code(500);
-                }
-            }
-        }
+        path: '/register',
+        handler: registerHandler,
     },
     {
         method: 'POST',
-        path: '/download',
-        handler: downloadHandler
+        path: '/login',
+        handler: loginHandler,
+    },
+    {
+        method: 'POST',
+        path: '/forgotPassword',
+        handler: forgotPassword,
     }
 ];
 
